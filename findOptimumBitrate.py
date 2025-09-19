d95b8fb3da98c84877574609a998b1cbb20ae0d2
Pradeep A <pradeepantapnal@users.noreply.github.com>
2025-09-19 13:10:29 +0530

    Add files via upload

---
+++ findOptimumBitrate.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""Utilities for finding the optimum bitrate of videos with FFmpeg.
+
+
+Run ``python findOptimumBitrate.py --help`` for a full list of options.
+"""
+
+__author__ = 'Pradeep Antapnal'
+VERSION = "1.0.0"
